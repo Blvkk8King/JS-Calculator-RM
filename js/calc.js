fabf4783869b701028e8fd823cf9fbbb85e1b395
Blvkk8King <glomadness300@gmail.com>
2022-12-12 14:12:26 -0500

    Initial commit

---
+++ js/calc.js
@@ -0,0 +1,19 @@
+let result = document.getElementById('result');
+
+function addNum(){
+    let firstNum = 2;
+    let secondNum = 2;
+    result.innerHTML = firstNum + secondNum;
+}
+
+function subNum(){
+    let thirdNum = 8;
+    let fourthNum = 6;
+    result.innerHTML = thirdNum - fourthNum;
+}
+
+function divNum(){
+    let fifthNum = 25;
+    let sixthNum = 5;
+    result.innerHTML = fifthNum / sixthNum;
+}
